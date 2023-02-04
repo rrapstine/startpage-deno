8d4a12362797b5a85b9924d1182837ba1f582a8f
Richard Rapstine <richard.rapstine@gmail.com>
2023-02-03 21:54:07 -0600

    Add twind config

--- src/twind/twind.config.ts
+++ src/twind/twind.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from "@twind/core";
-import presetAutoprefix from "@twind/preset-autoprefix";
-import presetTailwind from "@twind/preset-tailwind";
+import { defineConfig } from '@twind/core';
+import presetAutoprefix from '@twind/preset-autoprefix';
+import presetTailwind from '@twind/preset-tailwind';
 
 export default defineConfig({
   theme: {
-    // add theme styles here
+    extend: {
+      fontSize: {
+        lg: '1.5rem',
+      },
+      backgroundImage: {
+        home: "url('./src/assets/images/bg-image.webp')",
+      },
+      colors: {
+        gray: '#BBBBBB',
+      },
+    },
   },
   presets: [presetAutoprefix(), presetTailwind()],
 });
